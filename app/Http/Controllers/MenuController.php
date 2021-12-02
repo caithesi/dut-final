59a282be7a00a121834d74d47e4fc1d406fd5d82
sict <sict@bap.jp>
2021-12-02 21:25:59 +0700

    login admin

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -11,6 +11,10 @@ use Illuminate\Support\Str;
 
 class MenuController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware("auth");
+    }
     /**
      * Display a listing of the resource.
      *
